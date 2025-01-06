2339d1988bfbf2f15b42156ba27f28cd88de0757
jdbrigham <jbrigham@mac.com>
2025-01-06 07:10:12 -0500

    003

---
+++ 003/js/script.js
@@ -0,0 +1,294 @@
+/*Jan-3*/var  w1 = [8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Jan-10*/var  w2 = [0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Jan-17*/var  w3 = [0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Jan-24*/var  w4 = [0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Jan-31*/var  w5 = [0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Feb-7*/var  w6 = [0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Feb-14*/var  w7 = [0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Feb-21*/var  w8 = [0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Feb-28*/var  w9 = [0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Mar-7*/var  w10 = [0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Mar-14*/var  w11 = [0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Mar-21*/var  w12 = [0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Mar-28*/var  w13 = [0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Apr-4*/var  w14 = [0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Apr-11*/var  w15 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Apr-18*/var  w16 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Apr-25*/var  w17 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*May-2*/var  w18 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*May-9*/var  w19 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*May-16*/var  w20 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*May-23*/var  w21 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*May-30*/var  w22 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Jun-6*/var  w23 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Jun-13*/var  w24 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Jun-20*/var  w25 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Jun-27*/var  w26 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Jul-4*/var  w27 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Jul-11*/var  w28 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Jul-18*/var  w29 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Jul-25*/var  w30 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Aug-1*/var  w31 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Aug-8*/var  w32 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Aug-15*/var  w33 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Aug-22*/var  w34 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Aug-29*/var  w35 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Sep-5*/var  w36 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Sep-12*/var  w37 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Sep-19*/var  w38 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Sep-26*/var  w39 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Oct-3*/var  w40 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0]
+/*Oct-10*/var  w41 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0]
+/*Oct-17*/var  w42 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0]
+/*Oct-24*/var  w43 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0]
+/*Oct-31*/var  w44 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0]
+/*Nov-7*/var  w45 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0]
+/*Nov-14*/var  w46 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0]
+/*Nov-21*/var  w47 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0]
+/*Nov-28*/var  w48 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0]
+/*Dec-5*/var  w49 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0]
+/*Dec-12*/var  w50 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0]
+/*Dec-19*/var  w51 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0]
+/*Dec-26*/var  w52 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8]
+
+var ctx = document.getElementById('myChart').getContext("2d");
+
+var myLabels = ["1/1","1/8","1/15","1/22","1/29","2/5","2/12","2/19","2/26","3/4","3/11","3/18","3/25","4/1","4/8","4/15","4/22","4/29","5/6","5/13","5/20","5/27","6/3","6/10","6/17","6/24","7/1","7/8","7/15","7/22","7/29","8/5","8/12","8/19","8/26","9/2","9/9","9/16","9/23","9/30","10/7","10/14","10/21","10/28","11/4","11/11","11/18","11/25","12/2","12/9","12/16","12/23","12/30"]
+
+// hard code the weeknumber value (record number) from the list of labels - update each week
+var myWeekNum = myLabels [16];
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+			labels: myLabels,
+        datasets: [{
+            label: "Minutes of Daylight",
+            borderColor: "#fff",
+
+			pointBackgroundColor: "crimson",
+
+            pointBorderColor: [],
+/*
+            pointHoverBackgroundColor: [],
+            pointHoverBorderColor: [],
+            pointBorderWidth: [],
+            pointHoverRadius: 0,
+            pointHoverBorderWidth: 1,
+            pointStyle: ['dash','circle','star'],
+*/
+            fill: false,
+/*
+            borderWidth: 1,
+ */
+             data: ["554","563","575","589","606","623","642","661","681","701","721","741","761","781","800","819","837","854","870","884","896","906","912","916","916","914","908","899","888","875","860","843","826","807","788","769","749","729","709","690","670","651","632","614","597","582","569","558","550","545","544","546"]
+
+        }]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        responsive: true,
+        maintainAspectRatio: true,
+        tooltips: {
+         enabled: true
+       },
+       elements: {
+         point: {
+           radius: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+         }
+       },
+        title: {
+            display: false,
+        },
+        animation: {
+        duration: 0, // general animation time
+    },
+    annotation: {
+   drawTime: 'afterDatasetsDraw',
+   annotations: [{
+      id: 'hline1',
+      type: 'line',
+      mode: 'horizontal',
+      scaleID: 'y-axis-0',
+      value: "721",
+      borderColor: '#fff',
+      borderWidth: 1,
+      borderDash: [2, 12],
+      borderDashOffset: 0,
+      label: {
+         content: "EQUINOX",
+         backgroundColor: 'white',
+         fontColor: "#555",
+         enabled: true,
+         cornerRadius: 6,
+         position: "center",
+         xAdjust: -15,
+      }
+   },  {
+      id: 'vline2',
+      type: 'line',
+      mode: 'vertical',
+      scaleID: 'x-axis-0',
+      value: myWeekNum,
+      borderColor: '#339966',
+      borderWidth: .01,
+      label: {
+         backgroundColor: "red",
+         content: "TODAY",
+         enabled: false,
+         position: "top"
+      }
+   }]
+},
+        scales: {
+            pointLabels :{
+           fontStyle: "bold",
+        },
+        yAxes: [{
+                ticks: {
+                    display: false,
+                    fontColor: "rgba(0,0,0,0.5)",
+                    fontStyle: "bold",
+                    beginAtZero: false,
+                    maxTicksLimit: 5,
+                    padding: 20
+                },
+                scaleLabel: {
+        display: false,
+        labelString: 'Day Length In Minutes',
+        fontSize: 24,
+        fontColor: "#E0E0E0",
+        fontFamily: 'Arial',
+        fontStyle: 'bold'
+      },
+      gridLines: {
+                    drawTicks: false,
+                    display: false
+                }
+
+            }],
+            xAxes: [{
+                gridLines: {
+                  drawTicks: false,
+                  display: false
+                },
+                scaleLabel: {
+        display: false,
+        labelString: 'Week Number of the Year',
+        fontSize: 24,
+        fontColor: '#E0E0E0',
+        fontFamily: 'Arial',
+        fontStyle: 'bold'
+      },
+                ticks: {
+                    display: false,
+                    padding: 20,
+                    fontColor: "rgba(0,0,0,0.5)",
+                    fontStyle: "bold"
+                }
+            }]
+        }
+    }
+});
+
+// function to get the date of next Monday
+Date.prototype.getNextWeekMonday = function() {
+    var d = new Date(this.getTime());
+    var diff = d.getDate() - d.getDay() + 1;
+    if (d.getDay() == 0)
+        diff -= 7;
+    diff += 7; // ugly hack to get next monday instead of current one
+    return new Date(d.setDate(diff));
+};
+
+//set a new variable to get the weekday name of today
+var weekd=new Date()
+var weekday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday",
+            "Friday","Saturday")
+var myweekday = weekday[weekd.getDay()]
+
+// use a separate variable for figuring out the past date
+	var ourDate = new Date();
+
+// use an if loop to check if today is a Monday, Tuesday or Wednesday and adjust the results accordingly
+if (myweekday == 'Monday' || myweekday == 'Tuesday' || myweekday == 'Wednesday') {
+
+	//Change ourDate so that it is 7 days in the past.
+	var pastDate = ourDate.getDate() - 7;
+	ourDate.setDate(pastDate);
+}
+else
+var ourDate = ourDate;
+
+
+// set the variables for using the results of above based on ourDate
+var theNextMonday = (ourDate.getNextWeekMonday());
+var theMonth = theNextMonday.getMonth() + 1;
+var theDay = theNextMonday.getDate();
+var myNextMonday = theMonth + "/" + theDay;
+var myW = w1;
+
+document.getElementById('footnote').innerHTML ="Updated on Mondays (closest Monday is: " + myNextMonday + ")";
+// now that I know whether to use the coming or the previous Monday, I use an if loop to set a variable for the dot itself
+
+// I used an Excel file to generate the if statements
+if (myNextMonday == '1/1') myW = w1;
+else if (myNextMonday == '1/8') myW = w2;
+else if (myNextMonday == '1/15') myW = w3;
+else if (myNextMonday == '1/22') myW = w4;
+else if (myNextMonday == '1/29') myW = w5;
+else if (myNextMonday == '2/5') myW = w6;
+else if (myNextMonday == '2/12') myW = w7;
+else if (myNextMonday == '2/19') myW = w8;
+else if (myNextMonday == '2/26') myW = w9;
+else if (myNextMonday == '3/4') myW = w10;
+else if (myNextMonday == '3/11') myW = w11;
+else if (myNextMonday == '3/18') myW = w12;
+else if (myNextMonday == '3/25') myW = w13;
+else if (myNextMonday == '4/1') myW = w14;
+else if (myNextMonday == '4/8') myW = w15;
+else if (myNextMonday == '4/15') myW = w16;
+else if (myNextMonday == '4/22') myW = w17;
+else if (myNextMonday == '4/29') myW = w18;
+else if (myNextMonday == '5/6') myW = w19;
+else if (myNextMonday == '5/13') myW = w20;
+else if (myNextMonday == '5/20') myW = w21;
+else if (myNextMonday == '5/27') myW = w22;
+else if (myNextMonday == '6/3') myW = w23;
+else if (myNextMonday == '6/10') myW = w24;
+else if (myNextMonday == '6/17') myW = w25;
+else if (myNextMonday == '6/24') myW = w26;
+else if (myNextMonday == '7/1') myW = w27;
+else if (myNextMonday == '7/8') myW = w28;
+else if (myNextMonday == '7/15') myW = w29;
+else if (myNextMonday == '7/22') myW = w30;
+else if (myNextMonday == '7/29') myW = w31;
+else if (myNextMonday == '8/5') myW = w32;
+else if (myNextMonday == '8/12') myW = w33;
+else if (myNextMonday == '8/19') myW = w34;
+else if (myNextMonday == '8/26') myW = w35;
+else if (myNextMonday == '9/2') myW = w36;
+else if (myNextMonday == '9/9') myW = w37;
+else if (myNextMonday == '9/16') myW = w38;
+else if (myNextMonday == '9/23') myW = w39;
+else if (myNextMonday == '9/30') myW = w40;
+else if (myNextMonday == '10/7') myW = w41;
+else if (myNextMonday == '10/14') myW = w42;
+else if (myNextMonday == '10/21') myW = w43;
+else if (myNextMonday == '10/28') myW = w44;
+else if (myNextMonday == '11/4') myW = w45;
+else if (myNextMonday == '11/11') myW = w46;
+else if (myNextMonday == '11/18') myW = w47;
+else if (myNextMonday == '11/25') myW = w48;
+else if (myNextMonday == '12/2') myW = w49;
+else if (myNextMonday == '12/9') myW = w50;
+else if (myNextMonday == '12/16') myW = w51;
+else if (myNextMonday == '12/23') myW = w52;
+else if (myNextMonday == '12/30') myW = w53;
+else
+  document.write("<p>none of the conditions were met</p>");
+
+// here I use the results to update the dot (pointRadius) on the chart
+myChart.data.datasets[0].pointRadius = myW;
+    myChart.update();
